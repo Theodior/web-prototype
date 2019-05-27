6007761d676690ac93c162ea527963b27e0bf21c
Théodior <Theotasticque@laptop-8b9nhgqt.kea.local>
2019-05-27 12:46:04 +0200

    Afleveringsarrow

--- script.js
+++ script.js
@@ -24,3 +24,31 @@ function toggleMenu() {
         document.querySelector("#menuknap").textContent = "X";
     }
 }
+
+
+/*Arrowmenu*/
+window.addEventListener("load", afleveringVises);
+
+
+function afleveringVises() {
+    console.log("afleveringVises");
+
+    document.querySelector("#arrowknap").addEventListener("click", toggleArrowMenu);
+
+
+}
+
+
+function toggleArrowMenu() {
+    console.log("toggleArrowMenu");
+    document.querySelector("#arrowmenu").classList.toggle("hidden");
+
+    let erSkjult =
+        document.querySelector("#arrowmenu").classList.contains("hidden");
+
+    if (erSkjult == true) {
+        document.querySelector("#arrowknap").textContent = "▼";
+    } else {
+        document.querySelector("#arrowknap").textContent = "▲";
+    }
+}
